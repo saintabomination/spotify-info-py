08a9fa2534281b53beea82347d73b6e6a24fb87e
saintabomination <saint.abomination@protonmail.com>
2022-09-29 18:59:11 +0200

    Reading the access token from .env file

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from dotenv import dotenv_values
+
+ENV_PATH = '.env'
+
+env_file = dotenv_values(ENV_PATH)
+access_token = env_file['ACCESS_TOKEN']
+print(access_token)
+
 def main():
     print('Hello, World!')
 
