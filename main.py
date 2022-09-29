655be93333d4c703b97096f106c1a2f477ca8774
saintabomination <saint.abomination@protonmail.com>
2022-09-29 19:05:16 +0200

    Making a GET request to the Spotify API

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from dotenv import dotenv_values
+import requests
 
 ENV_PATH = '.env'
+ENV_FILE = dotenv_values(ENV_PATH)
+ACCESS_TOKEN = ENV_FILE['ACCESS_TOKEN']
+API_ENDPOINT = 'https://api.spotify.com/v1/me/player/currently-playing'
 
-env_file = dotenv_values(ENV_PATH)
-access_token = env_file['ACCESS_TOKEN']
-print(access_token)
+def request_current_song():
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}'
+    }
+    request = requests.get(API_ENDPOINT, headers = headers)
+
+    if request.status_code == 200:
+        data = request.json()
+        print(data)
+    else:
+        return 'N/A'
 
 def main():
-    print('Hello, World!')
+    request_current_song()
 
 if __name__ == '__main__':
     main()
